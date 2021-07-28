57bb8bacc8f40e3eb44c09c9562beb4ec1f02238
Yuki Hamanaka <yuki.hamanaka@10share.com>
2021-07-28 16:21:50 -0600

    テンプレ更新

--- webpack.config.js
+++ webpack.config.js
@@ -37,20 +37,10 @@ module.exports = {
     rules: [
       {
         test: /\.m?js$/,
-        exclude: /(node_modules)/,
         use: {
           loader: "babel-loader",
           options: {
-            presets: [
-              [
-                "@babel/preset-env",
-                {
-                  useBuiltIns: "entry",
-                  corejs: "3",
-                },
-              ],
-              "@babel/preset-react",
-            ],
+            presets: ["@babel/preset-env", "@babel/preset-react"],
             plugins: ["@babel/plugin-syntax-dynamic-import"],
           },
         },
